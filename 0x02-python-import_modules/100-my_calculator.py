43ba3dc7687e47d3ee8573d3ca3fe3a00c61489d
YusufYetunde <yusufyetunde13@gmail.com>
2023-06-09 03:08:36 -0700

    100 calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-
-from sys import argv, exit as sys_exit
-
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
-
 if __name__ == "__main__":
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    try:
-        a, op, b = argv[1:]
-    except ValueError:
+    argc = len(argv) - 1
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys_exit(1)
-        try:
-            print(a, op, b, "=", ops[op](int(a), int(b)))
-        except KeyError:
+        exit(1)
+        operator = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
+        if operator == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif operator == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif operator == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif operator == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        else:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys_exit(1)
-        except ValueError:
-            print("Invalid operand. Must be an integer")
-            sys_exit(1)
-        except ZeroDivisionError:
-            print("Invalid operation. Cannot divide by zero")
-            sys_exit(1)
+            exit(1)
