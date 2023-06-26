31d0f9919b730a2ccdb3503ec466567e60f5c7e1
YusufYetunde <yusufyetunde13@gmail.com>
2023-06-26 12:05:31 -0700

    Task 3

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -8,7 +8,7 @@ if not has_been_print:
     has_been_print = safe_print_integer(value)
     if not has_been_print
     print("{} is not an integer".format(value))
-    value = "Holberton"
+    value = "School"
     has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
