4671d4225d4963985a1d03901dd2136e184f93ae
Yusuf Yetunde <yusufyetunde13@gmail.com>
2023-09-11 15:10:41 -0700

    inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+module for Square.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle (9-rectangle.py)"""
+
+    def __init__(self, size):
+        """Constructor method for square
+        Instantiation with size"""
+
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return string representation"""
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
